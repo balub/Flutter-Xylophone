b9825c91ec9c7a7a8eb6defdcf9ec9c2d117a389
Balu Babu <balub997@gmail.com>
2020-01-05 23:09:36 +0530

    added multiple buttons for multiple sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -11,15 +10,59 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: FlatButton(
-              onPressed: () {
-                AudioPlayer audioPlayer = AudioPlayer();
-                audioPlayer.play('note1.wav');
-              },
-              child: Text(
-                'Click me',
-                style: TextStyle(fontSize: 20),
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.redAccent,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note1.wav');
+                  },
+                ),
+                FlatButton(
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note2.wav');
+                  },
+                ),
+                FlatButton(
+                  color: Colors.greenAccent,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note3.wav');
+                  },
+                ),
+                FlatButton(
+                  color: Colors.yellowAccent,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note4.wav');
+                  },
+                ),
+                FlatButton(
+                  color: Colors.orangeAccent,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note5.wav');
+                  },
+                ),
+                FlatButton(
+                  color: Colors.pinkAccent,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note6.wav');
+                  },
+                ),
+                FlatButton(
+                  color: Colors.indigoAccent,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note7.wav');
+                  },
+                ),
+              ],
             ),
           ),
         ),
